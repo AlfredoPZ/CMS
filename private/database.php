316a23ef2472e4cfa05b8bf6ee34800b1aab94a3
AlfredoPZ <alfredo.zamora@globallogic.com>
2024-06-10 12:23:36 -0600

    Feat: error handling

--- private/database.php
+++ private/database.php
@@ -4,6 +4,7 @@ require_once("db_credentials.php");
 
 function db_connected() {
     $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    confirm_db_connect();
     return $connection;
 }
 
@@ -12,5 +13,18 @@ function db_disconnect($connection) {
         mysqli_close($connection);
     }
 } 
+
+function confirm_db_connect() {
+    if(mysqli_connect_errno()) {
+        $msg = "Database connection failed: " . mysqli_connect_error() . " (" . mysqli_connect_errno() . ")";
+        exit($msg);
+    }
+}
+
+function confirm_result_set($result_set) {
+    if(!$result_set) {
+        exit("Database query failed");
+    }
+}
 
 ?>
--- private/query_functions.php
+++ private/query_functions.php
@@ -2,9 +2,10 @@
 
     function find_all_subjects() {
         global $db;
-
         $sql = "SELECT * FROM subjects ORDER BY position ASC";
         $result = mysqli_query($db, $sql);
+        confirm_result_set($result);
         return $result;
     }
+
 ?>
