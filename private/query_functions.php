7a7fa92af8ae52f5e01437035086fe625a14a8dd
AlfredoPZ <alfredo.zamora@globallogic.com>
2024-06-12 11:32:00 -0600

    Feat: show page information by id

--- private/query_functions.php
+++ private/query_functions.php
@@ -71,12 +71,24 @@
         }
     }
 
+    // pages
+
     function find_all_pages() {
         global $db;
         $sql = "SELECT * FROM pages ORDER BY  subject_id, position ASC";
         $result = mysqli_query($db, $sql);
         confirm_result_set($result);
         return $result;
+    }
+
+    function find_page_by_id($id) {
+        global $db;
+        $sql = "SELECT * FROM pages WHERE id = '" . $id ."'";
+        $result = mysqli_query($db, $sql);
+        confirm_result_set($result);
+        $subject = mysqli_fetch_assoc($result);
+        mysqli_free_result($result);
+        return $subject;
     }
 
 ?>
--- public/staff/pages/show.php
+++ public/staff/pages/show.php
@@ -1,15 +1,36 @@
 <?php
-    require_once("../../../private/initialize.php");
-    $id = $_GET["id"] ?? "1";
-    $page_title = "Show Page: " . $id; 
+require_once("../../../private/initialize.php");
+$page_title = "Hello";
+
+$id = $_GET["id"] ?? "1";
+
+$page = find_page_by_id($id);
+
 ?>
 
 
-<?php include( SHARED_PATH . "/staff/header.php"); ?>
+<?php include(SHARED_PATH . "/staff/header.php"); ?>
 
 <main id="content">
-    <a href="<?php echo WWW_ROOT . "/staff/pages" ?>">&laquo;Back to list</a>
-    <p><? echo $id; ?></p>
+    <a class="back-link" href="<?php echo url_for('/staff/pages/index.php'); ?>">&laquo; Back to List</a>
+    <aside id="main-menu"></aside>
+    <h1>page: <?php echo h($page['menu_name']); ?></h1>
+
+    <div class="attributes">
+        <dl>
+            <dt>Menu Name</dt>
+            <dd><?php echo h($page['menu_name']); ?></dd>
+        </dl>
+        <dl>
+            <dt>Position</dt>
+            <dd><?php echo h($page['position']); ?></dd>
+        </dl>
+        <dl>
+            <dt>Visible</dt>
+            <dd><?php echo $page['visible'] == '1' ? 'true' : 'false'; ?></dd>
+        </dl>
+    </div>
+
 </main>
 
-<?php include( SHARED_PATH . "/staff/footer.php"); ?>
+<?php include(SHARED_PATH . "/staff/footer.php"); ?>
