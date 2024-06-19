fc5169c6243fded27f6abdae3a1eab3110c68064
AlfredoPZ <alfredo.zamora@globallogic.com>
2024-06-19 09:31:56 -0600

    Feat: scope page position by subject

--- public/staff/pages/new.php
+++ public/staff/pages/new.php
@@ -28,9 +28,7 @@ if (is_post_request()) {
   $page["content"] = "";
 }
 
-$page_set = find_pages_by_subject_id($page["subject_id"]);
-$page_count = mysqli_num_rows($page_set) + 1;
-mysqli_free_result($page_set);
+$page_count = count_pages_by_subject_id($page["subject_id"]) + 1;
 $page["position"] = $page_count;
 $subject_set = find_all_subjects();
 
